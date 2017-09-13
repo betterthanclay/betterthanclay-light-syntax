4244fb9ed101e69b5186839a9aac69f44a3e024f
Tom Wright <tom@inflatablecookie.com>
2017-09-13 15:14:41 +0100

    Fixed function call weight
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- test.php
+++ test.php
@@ -31,6 +31,19 @@ class Test extends core\Base implements MyInterface, \ArrayAccess {
 
     public function __construct() {
         some\other\Thing::go();
+
+        $query
+            ->countRelation('tasks')
+            ->countRelation('goals')
+            ->correlateRelation('tasks', 'COUNT(completionDate)', 'completedTasks')
+            ->correlate('SUM(time) as time')
+                ->from('axis://job/Log', 'log')
+                ->whereCorrelation('log.task', 'in', 'id')
+                    ->from('axis://job/Task', 'task')
+                    ->whereField('task.category', '=', 'category.id')
+                    ->endCorrelation()
+                ->endCorrelation()
+            ->importRelationBlock('project', 'link');
     }
 
     protected function _flaps(arch\IRequest $request): ?core\Uri {
